e05c98b64616a1e5ffd5c9a099fb000401c32551
firo <cfiro@qq.com>
2021-12-18 16:54:12 +0800

    导航栏嵌套路由完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,15 @@ import VueRouter from "vue-router";
 import Login from "../views/Login";
 import Register from "../views/Register";
 import Home from "../views/Home";
+import Test1 from "../views/Test1";
+import Test2 from "../views/Test2";
 // 安装路由
 Vue.use(VueRouter);
 
 // 配置路由/定义路由
+
 const routes = [
+  // 登录页面
   {
     // 路径
     path: "/",
@@ -15,16 +19,32 @@ const routes = [
     // 需要跳转的组件
     component: Login,
   },
+  // 注册页面
   {
     path: "/Register",
     name: "Register",
     component: Register,
   },
+  // Home 主页面
   {
     path: "/Home",
     name: "Home",
     component: Home,
+    children: [
+      {
+        path: "test1",
+        name: "Test1",
+        component: Test1,
+      },
+      {
+        path: "test2",
+        name: "Test2",
+        component: Test2,
+      },
+    ],
   },
+
+  
 ];
 // 创建实例
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>hello</div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+          <!-- 触发动作看好了 @select -->
+          <!-- 在这写一个router直接可以去路由器对相应路径跳转 -->
+        <el-menu router>
+          <el-submenu index="1">
+            <template slot="title"
+              ><i class="el-icon-location"></i>导航一</template
+            >
+            <el-menu-item-group>
+                <!-- 跟普通的嵌套路由不太一样 -->
+              <el-menu-item index="/Home/test1">选项1</el-menu-item>
+              <el-menu-item index="/Home/test2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    menuClick(index) {
+      this.$router.push(index);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
